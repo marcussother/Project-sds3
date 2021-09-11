ed853e855229c396235bab55490684279000c716
masrcussother <masrcussother@gmail.com>
2021-09-11 07:55:22 -0300

    Bassic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    
+    <div className="conteiner">
       <h1 className="text-primary">Olá Mundo!</h1>
+      <DataTable />
     </div>
+    <Footer />
+    
+    </>
   );
 }
 
